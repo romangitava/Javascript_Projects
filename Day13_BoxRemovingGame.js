0e709f174220e8267f98ef2c36307d5b3b0a3cae
Roman <romangit261@gmail.com>
2022-12-27 12:19:12 -0800

    Initial commit

---
+++ Day13_BoxRemovingGame.js
@@ -0,0 +1,309 @@
+// Get First Box
+let getFirstBox = document.getElementsByClassName("item")[0]
+//addEventListner
+getFirstBox.addEventListener("click", firstRemoveFunction)
+
+// For First Box
+function firstRemoveFunction(){
+    getFirstBox.removeAttribute("class")
+}
+
+
+// Get Second Box 
+let getSecondBox = document.getElementsByClassName("item1")[0]
+//addEventListener
+getSecondBox.addEventListener("click", secondRemoveFunction)
+
+// For Second Box
+function secondRemoveFunction(){
+    getSecondBox.removeAttribute("class")
+}
+
+
+// Get Third Box
+let getThirdBox = document.getElementsByClassName("item2")[0]
+//addEventListener
+getThirdBox.addEventListener("click", thirdRemoveFunction)
+
+function thirdRemoveFunction(){
+    getThirdBox.removeAttribute("class")
+}
+
+// Get Forth Box
+let getForthBox = document.getElementsByClassName("item3")[0]
+//addEventListener
+getForthBox.addEventListener("click", forthRemoveFunction)
+
+// For Forth Box
+function forthRemoveFunction(){
+    getForthBox.removeAttribute("class")
+}
+
+// Get Sixth Box
+let getSixthBox =  document.getElementsByClassName("item4")[0]
+// addEventListener 
+getSixthBox.addEventListener("click", sixthRemoveFunction)
+
+//For Sixth Box
+function sixthRemoveFunction(){
+    getSixthBox.removeAttribute("class")
+}
+
+
+//Get Seventh Box
+let getSeventhBox = document.getElementsByClassName("item5")[0]
+getSeventhBox.addEventListener("click", seventhRemoveFunction)
+
+//For Seventh Function
+function seventhRemoveFunction(){
+    getSeventhBox.removeAttribute("class")
+}
+
+// Get Eight Box
+let getEightBox = document.getElementsByClassName("item6")[0]
+getEightBox.addEventListener("click", eightRemoveBox)
+
+// For Eight Box
+function eightRemoveBox(){
+    getEightBox.removeAttribute("class")
+}
+
+
+
+// Get Ninth Box
+let getNinthBox = document.getElementsByClassName("item7")[0]
+getNinthBox.addEventListener("click", ninthRemoveFunction)
+
+// For Ninth Box
+function ninthRemoveFunction(){
+    getNinthBox.removeAttribute("class")
+}
+
+
+
+// Get Tenth Box
+let getTenthBox = document.getElementsByClassName("item8")[0]
+getTenthBox.addEventListener("click", tenthRemoveFunction)
+
+// For Tenth Box
+function tenthRemoveFunction(){
+    getTenthBox.removeAttribute("class")
+}
+
+
+
+// Get Eleventh Box
+let getEleventhBox = document.getElementsByClassName("item9")[0]
+getEleventhBox.addEventListener("click", eleventhRemoveFunction)
+
+// For Eleventh Box
+function eleventhRemoveFunction(){
+    getEleventhBox.removeAttribute("class")
+}
+
+
+
+// Get Twelth Box
+let getTwelthBox = document.getElementsByClassName("item10")[0]
+getTwelthBox.addEventListener("click", twelthRemoveFunction)
+
+// For Twelth Box
+function twelthRemoveFunction(){
+    getTwelthBox.removeAttribute("class")
+}
+
+
+
+// Get Thirteen Box
+let getThirteenBox = document.getElementsByClassName("item11")[0]
+getThirteenBox.addEventListener("click", thirteenRemoveFunction)
+
+// For Thirteen Box
+function thirteenRemoveFunction(){
+    getThirteenBox.removeAttribute("class")
+}
+
+
+
+// Get Forteen Box
+let getForteenBox = document.getElementsByClassName("item12")[0]
+getForteenBox.addEventListener("click", forteenRemoveFunction)
+
+// For Forteen Box
+function forteenRemoveFunction(){
+    getForteenBox.removeAttribute("class")
+}
+
+
+
+// Get Fifteen Box
+let getFifteenBox = document.getElementsByClassName("item13")[0]
+getFifteenBox.addEventListener("click", fifteenRemoveFunction)
+
+// For Fifteen Box
+function fifteenRemoveFunction(){
+    getFifteenBox.removeAttribute("class")
+}
+
+
+
+// Get Fifteen Box
+let getUniqueBox = document.getElementsByClassName("item14")[0]
+getUniqueBox.addEventListener("click", uniqueRemoveFunction)
+
+// For Fifteen Box
+function uniqueRemoveFunction(){
+    getUniqueBox.removeAttribute("class")
+}
+
+
+
+// Get Fifteen Box
+let getUnique1Box = document.getElementsByClassName("item15")[0]
+getUnique1Box.addEventListener("click", unique1RemoveFunction)
+
+// For Fifteen Box
+function unique1RemoveFunction(){
+    getUnique1Box.removeAttribute("class")
+}
+
+
+
+// Get Seventeen Box
+let getSeventeenBox = document.getElementsByClassName("item15")[0]
+getSeventeenBox.addEventListener("click", seveteenRemoveFunction)
+
+// For Seventeen Box
+function seveteenRemoveFunction(){
+    getSeventeenBox.removeAttribute("class")
+}
+
+
+
+// Get Eighteen Box
+let getEigteenBox = document.getElementsByClassName("item16")[0]
+getEigteenBox.addEventListener("click", eighteenRemoveFunction)
+
+// For Eighteen Box
+function eighteenRemoveFunction(){
+    getEigteenBox.removeAttribute("class")
+}
+
+
+// Get Ninteen Box
+let getNinteen = document.getElementsByClassName("item17")[0]
+getNinteen.addEventListener("click", ninteenRemoveFunction)
+
+// For Ninteen Box
+function ninteenRemoveFunction(){
+    getNinteen.removeAttribute("class")
+}
+
+
+// Get Twenty Box
+let getTwentyBox = document.getElementsByClassName("item18")[0]
+getTwentyBox.addEventListener("click", twentyRemoveFunction)
+
+// For Twenty Box
+function twentyRemoveFunction(){
+    getTwentyBox.removeAttribute("class")
+}
+
+
+// Get TwentyOne Box
+let getTwentyoneBox = document.getElementsByClassName("item19")[0]
+getTwentyoneBox.addEventListener("click", twentyoneRemoveFunction)
+
+// For TwentyOne Box
+function twentyoneRemoveFunction(){
+    getTwentyoneBox.removeAttribute("class")
+}
+
+
+// Get TwentyTwo Box
+let getTwentyTwoBox = document.getElementsByClassName("item20")[0]
+getTwentyTwoBox.addEventListener("click", twentyTwoRemoveFunction)
+
+// For TwentyTwo Box
+function twentyTwoRemoveFunction(){
+    getTwentyTwoBox.removeAttribute("class")
+}
+
+
+// Get TwentyThree Box
+let getTwentyThreeBox = document.getElementsByClassName("item21")[0]
+getTwentyThreeBox.addEventListener("click", twentyThreeRemoveFunction)
+
+// For TwentyThree Box
+function twentyThreeRemoveFunction(){
+    getTwentyThreeBox.removeAttribute("class")
+}
+
+
+// Get twentyFour Box
+let getTwentyFourBox = document.getElementsByClassName("item22")[0]
+getTwentyFourBox.addEventListener("click", twentyFourRemoveFunction)
+
+// For twentyFour Box
+function twentyFourRemoveFunction(){
+    getTwentyFourBox.removeAttribute("class")
+}
+// Get TwentyFive Box
+let getTwentyFive = document.getElementsByClassName("item23")[0]
+getTwentyFive.addEventListener("click", twentyFiveRemoveFunction)
+
+// For TwentyFive Box
+function twentyFiveRemoveFunction(){
+    getTwentyFive.removeAttribute("class")
+}
+// Get twentySix Box
+let getTwentySixBox = document.getElementsByClassName("item24")[0]
+getTwentySixBox.addEventListener("click", sixteenRemoveFunction)
+
+// For twentySix Box
+function sixteenRemoveFunction(){
+    getTwentySixBox.removeAttribute("class")
+}
+
+
+// Get twentySeven Box
+let getSixteenBox = document.getElementsByClassName("item25")[0]
+getSixteenBox.addEventListener("click", twentySeveteenRemoveFunction)
+
+// For twentySeven Box
+function twentySeveteenRemoveFunction(){
+    getSixteenBox.removeAttribute("class")
+}
+
+
+// Get twentyEighteen Box
+let getTwentyEigtheen = document.getElementsByClassName("item26")[0]
+getTwentyEigtheen.addEventListener("click", twentyEigtheenRemoveFunction)
+
+// For twentyEighteen Box
+function twentyEigtheenRemoveFunction(){
+    getTwentyEigtheen.removeAttribute("class")
+}
+
+
+// Get twentyNinteen Box
+let getNinteenBox = document.getElementsByClassName("item27")[0]
+getNinteenBox.addEventListener("click", twentyNinteenRemoveFunction)
+
+// For twentyNinteen Box
+function twentyNinteenRemoveFunction(){
+    getNinteenBox.removeAttribute("class")
+}
+
+
+// Get twentySeven Box
+let getThirteeen = document.getElementsByClassName("item28")[0]
+getThirteeen.addEventListener("click", ThirteenRemoveFunction)
+
+// For twentySeven Box
+function ThirteenRemoveFunction(){
+    getThirteeen.removeAttribute("class")
+}
+
+
+
